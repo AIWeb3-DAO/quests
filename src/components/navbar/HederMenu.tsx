73071b302c81abd830f853eb378531f3e12ee5e1
abdul-kabugu <kabuguabdul2@gmail.com>
2024-01-31 13:11:27 +0300

    fix:  header issue

--- src/components/navbar/HederMenu.tsx
+++ src/components/navbar/HederMenu.tsx
@@ -9,7 +9,7 @@ export default function HederMenu() {
   };
   return (
     <div className='flex gap-5  items-center justify-center flex-1'>
-         <Link href={`/`} className='bg-gray-700/65 py-2 px-6 rounded-xl'>
+         <Link href={`/`} className='bg-gray-800/65 py-2 px-6 rounded-xl'>
            <p>Mint</p>
          </Link>
           {
